208eba12509be29bcf519a3516e2a614abf618a5
alijalilov <92136722+MukhammadaliDadabaev@users.noreply.github.com>
2023-01-15 11:32:07 +0500

    uploading files func

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Application;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        if ($request->hasFile('avatar')) {
+            $name = $request->file('avatar')->getClientOriginalName();
+            $path = $request->file('avatar')->storeAs(
+                'files',
+                $name,
+                'public'
+            );
+        }
+
+        $request->validate([
+            'subject' => 'required|max:255',
+            'message' => 'required',
+            'file_url' => 'mimes:jpg,png,pdf,webp,gif',
+        ]);
+
+        $application = Application::create([
+            'user_id' => auth()->user()->id,
+            'subject' => $request->subject,
+            'message' => $request->message,
+            'file_url' => $path ?? null,
+        ]);
+
+        return back();
+    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -46,16 +46,16 @@
             <div class='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
               <div class='max-w-md mx-auto space-y-6'>
 
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('applications.store') }}" method="POST" enctype="multipart/form-data">
                   @csrf
                   <h2 class="text-2xl font-bold ">Submit your application</h2>
                   <hr class="my-6">
                   <label class="uppercase text-sm font-bold opacity-70">Subject</label>
-                  <input type="text" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded">
+                  <input type="text" name="subject" required class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded">
                   <label class="uppercase text-sm font-bold opacity-70">Message</label>
-                  <textarea class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none" name="message" cols="30" rows="5"></textarea>
+                  <textarea class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none" name="message" required cols="30" rows="5"></textarea>
                   <label class="uppercase text-sm font-bold opacity-70">File</label>
-                  <input type="file" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
+                  <input type="file" name="avatar" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
                   <input type="submit" class="py-3 px-6  bg-emerald-500 text-white font-medium rounded hover:bg-indigo-500 cursor-pointer ease-in-out duration-300" value="Send">
                 </form>
 
