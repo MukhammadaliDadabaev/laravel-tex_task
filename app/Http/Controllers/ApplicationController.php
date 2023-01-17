2c8f2bfd54b5709feb772bee57c56580ad6adcdd
alijalilov <92136722+MukhammadaliDadabaev@users.noreply.github.com>
2023-01-17 16:38:11 +0500

    add applications create only application 1 a day done

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Jobs\SendEmailJob;
 use App\Models\Application;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 
@@ -11,6 +12,10 @@ class ApplicationController extends Controller
 {
     public function store(Request $request)
     {
+        if ($this->checkDate()) {
+            return back()->with('error', 'You can create only application 1 a day...😎');
+        }
+
         if ($request->hasFile('avatar')) {
             $name = $request->file('avatar')->getClientOriginalName();
             $path = $request->file('avatar')->storeAs(
@@ -36,5 +41,20 @@ class ApplicationController extends Controller
         dispatch(new SendEmailJob($application));
 
         return back();
+    }
+
+    protected function checkDate()
+    {
+        if (is_null(auth()->user()->applications()->latest()->first())) {
+            return false;
+        }
+
+        $last_application = auth()->user()->applications()->latest()->first();
+        $last_app_date = Carbon::parse($last_application->created_at)->format('Y-m-d');
+        $today = Carbon::now()->format('Y-m-d');
+
+        if ($last_app_date == $today) {
+            return true;
+        }
     }
 }
